8c47c87a89c4f4588aaa099a5141bee3c8709902
SeregaSergo <sergey.khattu@gmail.com>
2022-04-27 13:11:28 +0300

    Deleted all trash files

--- keyboards/keyboards.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from click import command
-
-        
-final_keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Закончить опрос"),
-                                                    KeyboardButton(text="Вернуться")]],
-                                    resize_keyboard=True)
